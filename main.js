66e0d6cb75d5e950e85fc17e287a9c59a1133f28
Katrin Sooäär <katrin.sooaar@khk.ee>
2018-10-10 16:26:34 +0300

    Lisatud inkrement ja faktide näitamine

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 let numberInput = document.getElementById('numberInput');
 let output = document.getElementById('output');
-
+let incInput = document.querySelector('#incInput');
 const getfact = () => {
+    output.innerHTML = '';
     let number = numberInput.value;
+    let inc = incInput.value;
+    let baseUrl = 'http://numbersapi.com/'
+    let url = `${baseUrl}${number}..${parseInt(number) + parseInt(inc)}`;
     
-    if (number != '') {
-    axios.get('http://numbersapi.com/'+number)
+    if (number != '' && inc != '') {
+    axios.get(url)
         .then(response => {
-            output.innerText = response.data;
+            for(let item in response.data) {
+                let li = document.createElement('li');
+                li.innerText = response.data[item];
+                output.appendChild(li);
+            }
          })
         .catch(error => {
             console.log(error);
     })
-    }
-    
+    }  
 };
 
 numberInput.addEventListener('input', getfact);
+incInput.addEventListener('input', getfact);
