ab378f7516b6e19943a6b29f711765fa3d281ada
Katrin Sooäär <katrin.sooaar@khk.ee>
2018-10-10 14:32:13 +0300

    Lisatud html struktuur

---
+++ main.js
@@ -0,0 +1,6 @@
+let numberInput = document.getElementById('numberInput');
+let output = document.getElementById('output');
+numberInput.addEventListener('input', event => {
+    let number = event.target.value;
+    output.innerHTML = 
+});
